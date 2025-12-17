6927a1c12816a3f0e4b8bbf2144a9e78a3876236
GEST <georg.strassberger@googlemail.com>
2025-12-17 10:33:18 +0100

    fix: add class=overview-hidden to remove the scrollbars by animation

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -24,7 +24,7 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="overflow-hidden">
     <TheTwoColumnsLayout
       :leftColumnClass="'col-md-8 bg-vue'"
       :rightColumnClass="'col-md-4'"
